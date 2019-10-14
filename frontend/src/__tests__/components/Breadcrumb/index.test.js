155592c4018e7045331c6e6f72731e79b39e3f90
tagli <tagliati@gmail.com>
2019-10-14 11:14:10 -0300

    Add tests confs

---
+++ frontend/src/__tests__/components/Breadcrumb/index.test.js
@@ -0,0 +1,6 @@
+// import { render } from '@testing-library/react';
+import Breadcrumb from '~/components/Breadcrumb';
+
+describe('Breadcrumb component', () => {
+  it('should be able to display a breadcrumb', () => {});
+});
