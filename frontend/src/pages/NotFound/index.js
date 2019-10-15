c1007d71ab2f04813e3763fd444daf18c8640b01
tagli <tagliati@gmail.com>
2019-10-15 07:59:55 -0300

    remove unnecessary class

--- frontend/src/pages/NotFound/index.js
+++ frontend/src/pages/NotFound/index.js
@@ -4,9 +4,7 @@ import NotFoundBlock from './styles';
 const NotFound = () => (
   <NotFoundBlock>
     <h4>Página não encontrada</h4>
-    <a href="/" className="logo">
-      Ir para página principal
-    </a>
+    <a href="/">Ir para página principal</a>
   </NotFoundBlock>
 );
 
